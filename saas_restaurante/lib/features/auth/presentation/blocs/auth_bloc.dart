7a18271018411235644fee45bfd887e819317cf3
NicoleUg <sofia.ugarte@ucb.edu.bo>
2026-06-11 12:14:58 -0400

    feat: implemento AuthBloc para manejar los estados de autenticacion

---
+++ saas_restaurante/lib/features/auth/presentation/blocs/auth_bloc.dart
@@ -0,0 +1,52 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'auth_event.dart';
+import 'auth_state.dart';
+import '../../domain/usecases/login_usecase.dart';
+import '../../domain/usecases/check_auth_usecase.dart';
+import '../../domain/usecases/logout_usecase.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final LoginUseCase loginUseCase;
+  final CheckAuthUseCase checkAuthUseCase;
+  final LogoutUseCase logoutUseCase;
+
+  AuthBloc({
+    required this.loginUseCase,
+    required this.checkAuthUseCase,
+    required this.logoutUseCase,
+  }) : super(AuthInitial()) {
+    
+    on<CheckAuthRequested>(_onCheckAuthRequested);
+    on<LoginRequested>(_onLoginRequested);
+    on<LogoutRequested>(_onLogoutRequested);
+  }
+
+  Future<void> _onCheckAuthRequested(CheckAuthRequested event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+    try {
+      final user = await checkAuthUseCase.call();
+      if (user != null) {
+        emit(AuthAuthenticated(user));
+      } else {
+        emit(AuthUnauthenticated());
+      }
+    } catch (_) {
+      emit(AuthUnauthenticated());
+    }
+  }
+
+  Future<void> _onLoginRequested(LoginRequested event, Emitter<AuthState> emit) async {
+    emit(AuthLoading()); 
+    try {
+      final user = await loginUseCase.call(event.email, event.password);
+      emit(AuthAuthenticated(user)); 
+      emit(AuthError(e.toString())); 
+      emit(AuthUnauthenticated()); 
+    }
+  }
+
+  Future<void> _onLogoutRequested(LogoutRequested event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+    await logoutUseCase.call();
+    emit(AuthUnauthenticated()); 
+}
