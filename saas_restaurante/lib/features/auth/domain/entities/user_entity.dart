c50a2f8f52823f1c114672d332d96a0028cb9ed9
NicoleUg <sofia.ugarte@ucb.edu.bo>
2026-06-11 11:16:00 -0400

    feat: se agrego el modelo de dominio UserEntity para el feature de autenticación

---
+++ saas_restaurante/lib/features/auth/domain/entities/user_entity.dart
@@ -0,0 +1,11 @@
+class UserEntity {
+  final String id;
+  final String email;
+  final String fullName;
+  final String role;
+  UserEntity({
+    required this.id,
+    required this.email,
+    required this.fullName,
+    required this.role,
+  });
