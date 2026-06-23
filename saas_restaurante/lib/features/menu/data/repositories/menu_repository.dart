6436e05199fd8db9e872c5d7145ccbb041b591f1
NicoleUg <sofia.ugarte@ucb.edu.bo>
2026-06-22 21:02:00 -0400

    feat: implement MenuRepository for category and product data management

--- saas_restaurante/lib/features/menu/data/repositories/menu_repository.dart
+++ saas_restaurante/lib/features/menu/data/repositories/menu_repository.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import '../../../../Core/network/api_client.dart';
 import '../models/category_model.dart';
 import '../models/product_model.dart';
@@ -24,6 +25,43 @@ class MenuRepository {
       return data.map((json) => ProductModel.fromJson(json)).toList();
     } catch (e) {
       throw Exception('Error al cargar los productos: $e');
+    }
+  }
+
+  Future<void> createProductWithImage({
+    required String categoryId,
+    required String name,
+    required String description,
+    required double price,
+    required String? localImagePath, 
+  }) async {
+    try {
+      final Map<String, dynamic> formDataMap = {
+        'categoryId': categoryId,
+        'name': name,
+        'description': description,
+        'price': price.toString(),
+      };
+      if (localImagePath != null && localImagePath.isNotEmpty) {
+        final fileName = localImagePath.split('/').last;
+        formDataMap['image'] = await MultipartFile.fromFile(
+          localImagePath,
+          filename: fileName,
+        );
+      }
+
+      final formData = FormData.fromMap(formDataMap);
+      final response = await apiClient.dio.post(
+        '/menu/products',
+        data: formData,
+      );
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        print('Producto creado con éxito con su imagen: ${response.data}');
+      }
+    } on DioException catch (e) {
+      print('Error al subir producto e imagen: ${e.message}');
+      throw Exception('No se pudo crear el producto');
     }
   }
 }
